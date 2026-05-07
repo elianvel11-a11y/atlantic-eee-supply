8c27244b72c6b9b8bbe0b8f23a656e207b4816d5
Elian Velez <elianvel11@Elians-MacBook-Pro.local>
2026-05-07 17:50:00 -0500

    Finalize CMS architecture and corporate content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import Hero from '@/components/sections/Hero'
 import HomeOverview from '@/components/sections/HomeOverview'
+import MissionVision from '@/components/sections/MissionVision'
 import HomeProducts from '@/components/sections/HomeProducts'
 import HomeWhyUs from '@/components/sections/HomeWhyUs'
 import HomeBrands from '@/components/sections/HomeBrands'
 import HomeCTA from '@/components/sections/HomeCTA'
-import { getHero, getAboutContent, getBrands } from '../../sanity/lib/queries'
+import { getHero, getAboutContent, getMissionVision, getBrands } from '../../sanity/lib/queries'
 
 export default async function Page() {
-  const [hero, about, brands] = await Promise.all([
+  const [hero, about, mv, brands] = await Promise.all([
     getHero(),
     getAboutContent(),
+    getMissionVision(),
     getBrands(),
   ])
 
@@ -17,6 +19,7 @@ export default async function Page() {
     <main>
       <Hero cms={hero} />
       <HomeOverview cms={about} />
+      <MissionVision cms={mv} />
       <HomeProducts />
       <HomeWhyUs />
       <HomeBrands cms={brands} />
